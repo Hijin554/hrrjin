d7b550750a275df1b396f592822cfc109195a090
Hijin554 <dhgmlwls86@gmail.com>
2026-01-15 19:49:14 +0900

    feature scetion complit

--- js/coustom.js
+++ js/coustom.js
@@ -1,18 +1,5 @@
-/**Header Change Effect **/
-const header = document.querySelector('header');
-const stickyHeader = () => {
-  const scry = window.scrollY;
-  if (scry > 0) {
-    header.classList.add('active');
-  } else {
-    header.classList.remove('active');
-  }
-};
-
-window.addEventListener('scroll', stickyHeader);
-
 /**Scroll reveal 효과 **/
-const sr = scrollReveal({
+const sr = ScrollReveal({
   reset: false, //스크롤시 한번만 실행
 });
 
@@ -20,4 +7,15 @@ sr.reveal('.landing-text-box', {
   duration: 1000,
   origin: 'right',
   distance: '80px',
+});
+sr.reveal('.meet-text-box', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '40px',
+});
+sr.reveal('.meet-wrapper img, .feature', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '40px',
+  interval: 200, //0.2초 간격 차례로 효과진행
 });
